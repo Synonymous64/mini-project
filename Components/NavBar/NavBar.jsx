749786ff1aef79595f6d92a18abd4eb8da2879ca
Synonymous64 <prajwalurkude007@gmail.com>
2023-04-01 23:56:34 +0530

    intial setup and smart contract of messaging

---
+++ Components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Style from './Navbar.module.css';
+
+const NavBar = () => {
+    return (
+        <div>NavBar</div>
+    )
+}
+
+export default NavBar;
---
+++ Components/index.js
@@ -0,0 +1,9 @@
+import NavBar from "./NavBar/NavBar";
+import Filter from "./Filter/Filter";
+import Error from "./Error/Error";
+import Friend from "./Friend/Friend";
+import Model from "./Model/Model";
+import UserCard from "./UserCard/UserCard";
+import Loader from "./Loader/Loader";
+
+export { NavBar, Filter, Error, Friend, Model, UserCard, Loader };
